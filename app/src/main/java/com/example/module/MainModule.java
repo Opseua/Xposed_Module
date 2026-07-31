bcb52d8d24c9d0d636c7b86574c58422aef6df4a
Orlando <opseua@gmail.com>
2026-07-30 22:00:05 -0300

    Update MainModule.java

--- app/src/main/java/com/example/module/MainModule.java
+++ app/src/main/java/com/example/module/MainModule.java
@@ -33,24 +33,19 @@ public class MainModule extends XposedModule {
             );
 
             for (Method method : cameraCharacteristicsClass.getDeclaredMethods()) {
-                // Filtramos o método get() que recebe exatamente 1 argumento (a chave)
                 if (method.getName().equals("get") && method.getParameterTypes().length == 1) {
                     hook(method).intercept(chain -> {
-                        // Resgata o primeiro argumento passado para o método get()
-                        // Nota: Dependendo da versão do LibXposed, pode ser necessário usar chain.args()[0] ou chain.args[0]
-                        Object arg = chain.getArgs()[0]; 
+                        Object arg = chain.getArgs().get(0); 
 
                         if (arg instanceof CameraCharacteristics.Key) {
                             CameraCharacteristics.Key<?> key = (CameraCharacteristics.Key<?>) arg;
                             
-                            // Verifica se o nome da chave é o que queremos interceptar
                             if ("android.control.zoomRatioRange".equals(key.getName())) {
                                 Log.i(TAG, "📸 [Spoof] Alterando Zoom para 0.5 - 8.0 no pacote: " + packageName);
                                 return new Range<>(0.5f, 8.0f);
                             }
                         }
 
-                        // Para qualquer outra chave, executamos e retornamos o método original
                         return chain.proceed();
                     });
                 }
