69dbc5efc5821af8dcb7fa4f3b30dc60f388e9b0
Orlando <opseua@gmail.com>
2026-07-30 21:56:52 -0300

    Refactor camera hook logic in MainModule

--- app/src/main/java/com/example/module/MainModule.java
+++ app/src/main/java/com/example/module/MainModule.java
@@ -1,6 +1,8 @@
 package com.example.module;
 
+import android.hardware.camera2.CameraCharacteristics;
 import android.util.Log;
+import android.util.Range;
 
 import java.lang.reflect.Method;
 
@@ -21,50 +23,41 @@ public class MainModule extends XposedModule {
         super.onPackageReady(param);
 
         String packageName = param.getPackageName();
-
-        Log.i(TAG, "MODULO: APP ALVO " + packageName + " ABERTO");
+        Log.i(TAG, "MODULO: APP ALVO " + packageName + " INICIOU");
 
         try {
-            Class<?> cameraManagerClass = Class.forName(
-                    "android.hardware.camera2.CameraManager",
+            Class<?> cameraCharacteristicsClass = Class.forName(
+                    "android.hardware.camera2.CameraCharacteristics",
                     false,
                     param.getClassLoader()
             );
 
-            for (Method method : cameraManagerClass.getDeclaredMethods()) {
-                if (method.getName().equals("openCamera")) {
+            for (Method method : cameraCharacteristicsClass.getDeclaredMethods()) {
+                // Filtramos o método get() que recebe exatamente 1 argumento (a chave)
+                if (method.getName().equals("get") && method.getParameterTypes().length == 1) {
                     hook(method).intercept(chain -> {
-                        Object result = chain.proceed();
-                        Log.i(TAG, "MODULO: APP ALVO " + packageName + " USOU A CAMERA");
-                        return result;
+                        // Resgata o primeiro argumento passado para o método get()
+                        // Nota: Dependendo da versão do LibXposed, pode ser necessário usar chain.args()[0] ou chain.args[0]
+                        Object arg = chain.getArgs()[0]; 
+
+                        if (arg instanceof CameraCharacteristics.Key) {
+                            CameraCharacteristics.Key<?> key = (CameraCharacteristics.Key<?>) arg;
+                            
+                            // Verifica se o nome da chave é o que queremos interceptar
+                            if ("android.control.zoomRatioRange".equals(key.getName())) {
+                                Log.i(TAG, "📸 [Spoof] Alterando Zoom para 0.5 - 8.0 no pacote: " + packageName);
+                                return new Range<>(0.5f, 8.0f);
+                            }
+                        }
+
+                        // Para qualquer outra chave, executamos e retornamos o método original
+                        return chain.proceed();
                     });
                 }
             }
 
         } catch (Throwable t) {
             Log.e(TAG, "MODULO: erro ao hookar em " + packageName + ": " + t, t);
-        }
-
-        try {
-            Class<?> activityThreadClass = Class.forName(
-                    "android.app.ActivityThread",
-                    false,
-                    param.getClassLoader()
-            );
-
-            for (Method method : activityThreadClass.getDeclaredMethods()) {
-                if (method.getName().equals("handleDestroyActivity")
-                        || method.getName().equals("performDestroyActivity")) {
-                    hook(method).intercept(chain -> {
-                        Object result = chain.proceed();
-                        Log.i(TAG, "MODULO: APP ALVO " + packageName + " FECHADO");
-                        return result;
-                    });
-                }
-            }
-
-        } catch (Throwable t) {
-            Log.e(TAG, "MODULO: erro ao hookar fechamento em " + packageName + ": " + t, t);
         }
     }
 }
