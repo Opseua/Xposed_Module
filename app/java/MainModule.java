8bf5c60dfadfc592742442c8554a006643785d53
Orlando <opseua@gmail.com>
2026-08-15 19:27:57 -0300

    Update MainModule.java

--- app/java/MainModule.java
+++ app/java/MainModule.java
@@ -1,10 +1,8 @@
 package com.xposedmodule.module;
 
-import android.os.SystemClock;
 import android.util.Log;
 import dalvik.system.DexClassLoader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
@@ -13,12 +11,8 @@ import io.github.libxposed.api.XposedModule;
 
 public class MainModule extends XposedModule {
     private static final String TAG = "MODULO_LOADER";
-    private static final String BOOT_MARKER_FILE = "/data/data/%s/files/xposed/module_boot.txt";
+    private static final String LOG_FILE_PATH = "/data/data/%s/files/xposed/module_log.txt";
     private static final String IGNORE_FILE_PATH = "/storage/emulated/0/Pictures/xposed/module_ignore.txt";
-
-    private long instanteDoBootAtual() {
-        return System.currentTimeMillis() - SystemClock.elapsedRealtime();
-    }
 
     private boolean eProcessoPrincipal(String packageName) {
         try {
@@ -57,29 +51,9 @@ public class MainModule extends XposedModule {
             return;
         }
 
-        try {
-            long bootAtual = instanteDoBootAtual();
-            File marcador = new File(String.format(BOOT_MARKER_FILE, packageName));
-
-            long ultimoBootMarcado = -1;
-            if (marcador.exists()) {
-                String conteudo = new String(Files.readAllBytes(marcador.toPath())).trim();
-                try {
-                    ultimoBootMarcado = Long.parseLong(conteudo);
-                } catch (NumberFormatException ignored) {}
-            }
-
-            boolean bootDiferente = Math.abs(bootAtual - ultimoBootMarcado) > 2000;
-
-            if (bootDiferente) {
-                if (!marcador.getParentFile().exists()) marcador.getParentFile().mkdirs();
-                try (FileWriter w = new FileWriter(marcador, false)) {
-                    w.write(String.valueOf(bootAtual));
-                }
-                this.log(Log.INFO, TAG, "MODULO INICIADO");
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Falha ao checar/gravar marcador de boot: " + e.getMessage());
+        File logFile = new File(String.format(LOG_FILE_PATH, packageName));
+        if (!logFile.exists()) {
+            this.log(Log.INFO, TAG, "MODULO INICIADO");
         }
 
         File dexFile = new File("/storage/emulated/0/Pictures/xposed/server.dex");
