d869c7bad3f34a6314af4e2ec35d6028a80cdd92
Orlando <opseua@gmail.com>
2026-08-15 21:22:23 -0300

    Update MainModule.java

--- app/java/MainModule.java
+++ app/java/MainModule.java
@@ -1,11 +1,15 @@
 package com.xposedmodule.module;
 
+import android.hardware.camera2.CameraCharacteristics;
 import android.util.Log;
+import android.util.Range;
+import android.util.SizeF;
 import dalvik.system.DexClassLoader;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
+import java.util.Arrays;
 import io.github.libxposed.api.XposedModule;
 
 public class MainModule extends XposedModule {
@@ -37,8 +41,75 @@ public class MainModule extends XposedModule {
         return false;
     }
 
-    public Object hookMethodForPayload(Method method) {
-        return hook(method);
+    // Método que ativa o hook da câmera usando nativamente a API do libxposed
+    public void hookCameraCharacteristics(ClassLoader targetClassLoader, String packageName) {
+        try {
+            Class<?> cameraCharacteristicsClass = Class.forName(
+                    "android.hardware.camera2.CameraCharacteristics",
+                    false,
+                    targetClassLoader
+            );
+
+            for (Method method : cameraCharacteristicsClass.getDeclaredMethods()) {
+                if (method.getName().equals("get") && method.getParameterTypes().length == 1) {
+                    hook(method).intercept(chain -> {
+                        Object arg = chain.getArgs().get(0);
+
+                        if (arg instanceof CameraCharacteristics.Key) {
+                            CameraCharacteristics.Key<?> key = (CameraCharacteristics.Key<?>) arg;
+                            String keyName = key.getName();
+                            
+                            Object result = chain.proceed();
+                            
+                            if ("android.sensor.info.physicalSize".equals(keyName)) {
+                                SizeF spoof = new SizeF(7.6f, 5.7f);
+                                this.log(Log.INFO, TAG, "CAMERA: " + keyName + " -> " + spoof);
+                                return spoof;
+                            }
+                            
+                            if ("android.lens.info.availableFocalLengths".equals(keyName)) {
+                                float[] spoof = new float[] { 1.5f };
+                                this.log(Log.INFO, TAG, "CAMERA: " + keyName + " -> " + Arrays.toString(spoof));
+                                return spoof;
+                            }
+
+                            if ("android.sensor.info.timestampSource".equals(keyName)) {
+                                Integer spoof = 1;
+                                this.log(Log.INFO, TAG, "CAMERA: " + keyName + " -> " + spoof);
+                                return spoof;
+                            }
+
+                            if ("android.control.zoomRatioRange".equals(keyName)) {
+                                Range<Float> spoof = new Range<>(0.5f, 10.0f);
+                                this.log(Log.INFO, TAG, "CAMERA: " + keyName + " -> " + spoof);
+                                return spoof;
+                            }
+                            
+                            if ("android.request.availableCapabilities".equals(keyName)) {
+                                int[] spoof = new int[] {0, 1, 2, 3, 4, 5, 6, 8, 9, 11};
+                                this.log(Log.INFO, TAG, "CAMERA: " + keyName + " -> " + Arrays.toString(spoof));
+                                return spoof;
+                            }
+                            
+                            if ("android.control.aeAvailableTargetFpsRanges".equals(keyName)) {
+                                @SuppressWarnings("unchecked")
+                                Range<Integer>[] spoof = new Range[] {
+                                    new Range<>(15, 30), new Range<>(30, 30)
+                                };
+                                this.log(Log.INFO, TAG, "CAMERA: " + keyName + " -> " + Arrays.toString(spoof));
+                                return spoof;
+                            }
+
+                            return result;
+                        }
+
+                        return chain.proceed();
+                    });
+                }
+            }
+        } catch (Throwable t) {
+            this.log(Log.ERROR, TAG, "MODULO: erro ao hookar CameraCharacteristics em " + packageName + " -> " + t.getMessage());
+        }
     }
 
     @Override
